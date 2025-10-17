67a4416bc21ac4d13e3bf0ea7de9aa126d4c648f
Hossein Ghanbari <118113234+artsyl3@users.noreply.github.com>
2025-10-17 20:17:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
 
 <div className="app">
-      <h1>Reading Lissstszzzzzzzzzz</h1>
+      <h1>Reading Lissstz</h1>
       <BookList />
       <BookCreate />
     </div>
